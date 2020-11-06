c02db5b4cc89cb3049d5cc0642d7bafb71833170
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-07 02:34:50 +0700

    custom two way data binding

---
+++ day9-custom-two-way-data-binding/src/app/toggle/toggle.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css']
+})
+export class ToggleComponent implements OnInit {
+  @Input() checked: boolean;
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
