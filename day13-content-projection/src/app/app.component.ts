bcd4949da86dd03fac0a565fcea1c1b7b6287b8e
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-15 16:41:59 +0700

    content projection

---
+++ day13-content-projection/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  questions = {
+    question1: true,
+    question2: false
+  }
+}
